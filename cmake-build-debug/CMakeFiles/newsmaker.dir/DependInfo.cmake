4f6ee307bf4fe9c27a7dc0aa7c309ef4d26fee1e
Gosesummer <2953863750@qq.com>
2023-10-29 08:45:22 +0800

    new

---
+++ cmake-build-debug/CMakeFiles/newsmaker.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "F:/Gose/code_cpp/gosecpp/newsmaker/glad.c" "F:/Gose/code_cpp/gosecpp/newsmaker/cmake-build-debug/CMakeFiles/newsmaker.dir/glad.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "F:/Gose/code_cpp/gosecpp/newsmaker/main.cpp" "F:/Gose/code_cpp/gosecpp/newsmaker/cmake-build-debug/CMakeFiles/newsmaker.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/newsmaker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/newsmaker.dir/glad.c.obj"
+  "CMakeFiles/newsmaker.dir/main.cpp.obj"
+  "libnewsmaker.dll.a"
+  "newsmaker.exe"
+  "newsmaker.exe.manifest"
+  "newsmaker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/newsmaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
